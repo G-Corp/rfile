0d5f3b3320bd795ad51aa24d482b84a64b1831fb
glejeune <gregoire.lejeune@free.fr>
2018-04-09 15:53:08 +0200

    Update rfile_storage behabiour

--- src/rfile_storage.erl
+++ src/rfile_storage.erl
@@ -3,5 +3,8 @@
 -callback ls(File :: map(), Options :: rfile:options()) ->
   {ok, #{files => [string()], directories => [string()]}} | {error, term()}.
 
+-callback rm(File :: map(), Options :: rfile:options()) ->
+  {ok, string()} | {error, term()}.
+
 -callback copy(Spurce :: map(), Destination :: map(), Options :: rfile:options()) ->
   {ok, string()} | {error, term()}.
